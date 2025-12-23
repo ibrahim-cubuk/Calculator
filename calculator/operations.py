92cf14561f581f89cc85a91dd7f304ae10fa7070
ibrahim-cubuk <ibrahim.cubuk@motopp.nl>
2025-12-23 16:11:42 +0100

    bestanden toegevoegd van zipfile

---
+++ calculator/operations.py
@@ -0,0 +1,37 @@
+"""
+operations.py
+
+This file contains ALL calculator operations that the team will build.
+
+IMPORTANT:
+- All developers work in THIS SAME FILE.
+- Each developer completes ONLY their own function.
+- You are NOT allowed to move your function to a different file.
+
+This shared file is intentional. You will probably create merge conflicts.
+That's part of the exercise.
+"""
+
+
+def add(a, b):
+    """Return the sum of a and b. Developer A owns this function."""
+    return a + b
+
+
+
+def sub(a, b):
+    """Return a minus b. Developer B owns this function."""
+    return a - b
+
+
+
+def mul(a, b):
+    """Return the product of a and b. Developer C owns this function."""
+    return a * b
+
+
+
+def div(a, b):
+    """Return a divided by b. Developer D owns this function."""
+    return a / b
+
